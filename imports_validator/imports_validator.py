c901ec7a3139bd9531fabd887712e14fc97e3ed5
Michal <mskrzypa0@gmail.com>
2023-12-24 20:14:38 +0100

    better imports validator

--- imports_validator/imports_validator.py
+++ imports_validator/imports_validator.py
@@ -37,51 +37,97 @@ def get_output_filename(stage: str, run_params: Dict[str, str]) -> str:
         filename = f"{run_params['model']}_{run_params['params']}"
     return filename+".txt"
 
-def validate(stage: str, stage_params: List[Dict[str, str]], md5_df: pd.DataFrame):
-    for run_params in stage_params:
-        for item in run_params.items():
-            if item[0] == "params":
-                imports = get_imports(os.path.join("params", f"{item[1]}.yaml"))
-            else:
-                imports = get_imports(os.path.join("stages", f"{item[0]}s", f"{item[1]}.py"))
-            imports = [imp.replace(".", "/")+".py" for imp in imports]
-            custom_imports = [imp for imp in imports if is_file(imp)]
-            create_file_flag = False
-            for imp in custom_imports:
-                with open(imp, 'rb') as file:
-                    file_content = file.read()
-                    current_md5 = hashlib.md5(file_content).hexdigest()
-                    row = md5_df[md5_df['file'] == imp]
-                    if row.empty:
-                        create_file_flag = True
-                        md5_df = pd.concat([md5_df, pd.DataFrame({
-                            "file": [imp],
-                            "md5": [current_md5]
-                        })], ignore_index=True)
-                    else:
-                        last_md5 = row['md5'].values[0]
-                        if last_md5 != current_md5:
-                            create_file_flag = True
-                            md5_df.loc[md5_df['file'] == imp, 'md5'] = current_md5
-            
-            if create_file_flag:
-                path = os.path.join("imports_validator", stage)
-                if not os.path.exists(path):
-                    os.mkdir(path)
-                with open(os.path.join(path, get_output_filename(stage, run_params)), 'w') as file:
-                    logger.info(f"Creating file {os.path.join(path, get_output_filename(stage, run_params))}")
-                    file.write(str(time.time()))
-                    
-                    
+def _get_important_param_name(stage: str) -> str:
+    if stage == "load":
+        return "dataloader"
+    if stage == "clean":
+        return "datacleaner"
+    if stage == "vectorize":
+        return "vectorizer"
+    if stage == "evaluate":
+        return "model"
+    raise ValueError(f"Stage {stage} does not exsist.")
 
-            # if any(os.path.getmtime(c_imp) > output_mtime for c_imp in custom_imports):
-            #     path = os.path.join("imports_validator", stage)
-            #     if not os.path.exists(path):
-            #         os.mkdir(path)
-            #     with open(os.path.join(path, get_output_filename(stage, run_params)), 'w') as file:
-            #         logger.info(f"Creating directory {os.path.join(path, get_output_filename(stage, run_params))}")
-            #         file.write(str(time.time()))
-    return md5_df
+
+def validate(stage: str, stage_params: List[Dict[str, str]], md5_df: pd.DataFrame):
+    new_md5_df = pd.DataFrame(columns = ['file', 'md5'])
+    for run_params in stage_params:
+        param_name = _get_important_param_name(stage)
+        param_value = run_params[param_name]
+        imports = get_imports(os.path.join("stages", f"{param_name}s", f"{param_value}.py"))
+        imports = [imp.replace(".", "/")+".py" for imp in imports]
+        custom_imports = [imp for imp in imports if is_file(imp)]
+        create_file_flag = False
+        for imp in custom_imports:
+            with open(imp, 'rb') as file:
+                file_content = file.read()
+                current_md5 = hashlib.md5(file_content).hexdigest()
+                row = md5_df[md5_df['file'] == imp]
+                if row.empty:
+                    create_file_flag = True
+                    new_md5_df = pd.concat([new_md5_df, pd.DataFrame({
+                        "file": [imp],
+                        "md5": [current_md5]
+                    })], ignore_index=True)
+                else:
+                    last_md5 = row['md5'].values[0]
+                    if last_md5 != current_md5:
+                        create_file_flag = True
+                        new_md5_df = pd.concat([new_md5_df, pd.DataFrame({
+                        "file": [imp],
+                        "md5": [current_md5]
+                        })], ignore_index=True)
+        
+        if create_file_flag:
+            path = os.path.join("imports_validator", stage)
+            if not os.path.exists(path):
+                os.mkdir(path)
+            with open(os.path.join(path, get_output_filename(stage, run_params)), 'w') as file:
+                logger.info(f"Creating file {os.path.join(path, get_output_filename(stage, run_params))}")
+                file.write(str(time.time()))
+
+    return new_md5_df
+
+# def validate(stage: str, stage_params: List[Dict[str, str]], md5_df: pd.DataFrame):
+#     new_md5_df = pd.DataFrame(columns = ['file', 'md5'])
+#     for run_params in stage_params:
+#         for item in run_params.items():
+#             if item[0] == "params":
+#                 imports = get_imports(os.path.join("params", f"{item[1]}.yaml"))
+#             else:
+#                 imports = get_imports(os.path.join("stages", f"{item[0]}s", f"{item[1]}.py"))
+#             imports = [imp.replace(".", "/")+".py" for imp in imports]
+#             custom_imports = [imp for imp in imports if is_file(imp)]
+#             create_file_flag = False
+#             for imp in custom_imports:
+#                 with open(imp, 'rb') as file:
+#                     file_content = file.read()
+#                     current_md5 = hashlib.md5(file_content).hexdigest()
+#                     row = md5_df[md5_df['file'] == imp]
+#                     if row.empty:
+#                         create_file_flag = True
+#                         new_md5_df = pd.concat([new_md5_df, pd.DataFrame({
+#                             "file": [imp],
+#                             "md5": [current_md5]
+#                         })], ignore_index=True)
+#                     else:
+#                         last_md5 = row['md5'].values[0]
+#                         if last_md5 != current_md5:
+#                             create_file_flag = True
+#                             new_md5_df = pd.concat([new_md5_df, pd.DataFrame({
+#                             "file": [imp],
+#                             "md5": [current_md5]
+#                             })], ignore_index=True)
+            
+#             if create_file_flag:
+#                 path = os.path.join("imports_validator", stage)
+#                 if not os.path.exists(path):
+#                     os.mkdir(path)
+#                 with open(os.path.join(path, get_output_filename(stage, run_params)), 'w') as file:
+#                     logger.info(f"Creating file {os.path.join(path, get_output_filename(stage, run_params))}")
+#                     file.write(str(time.time()))
+
+#     return new_md5_df
 
 def main():
     with open('./params.yaml', 'r') as file:
@@ -90,11 +136,28 @@ def main():
         md5_df = pd.read_parquet('./imports_validator/md5.pickle')
     else:
         md5_df = pd.DataFrame(columns = ['file', 'md5'])
-    md5_df = validate("load", params['load'], md5_df)
-    md5_df = validate("clean", params['clean'], md5_df)
-    md5_df = validate("vectorize", params['vectorize'], md5_df)
-    md5_df = validate("evaluate", params['models'], md5_df)
-    md5_df.to_parquet('./imports_validator/md5.pickle')
+    
+    new_md5_dfs = []
+    new_md5_dfs.append(validate("load", params['load'], md5_df))
+    new_md5_dfs.append(validate("clean", params['clean'], md5_df))
+    new_md5_dfs.append(validate("vectorize", params['vectorize'], md5_df))
+    new_md5_dfs.append(validate("evaluate", params['models'], md5_df))
+
+    new_md5_df = pd.concat(new_md5_dfs)
+    new_md5_df = new_md5_df.drop_duplicates(subset = 'file')
+
+    # Iteruj po każdym wierszu w md5_df
+    for index, row in md5_df.iterrows():
+        file_value = row['file']
+
+        # Sprawdź, czy istnieje wiersz o tym samym pliku w new_md5_df
+        if new_md5_df[new_md5_df['file'] == file_value].empty:
+            # Jeśli nie istnieje, dodaj ten wiersz do new_md5_df
+            new_row = {'file': file_value, 'md5': row['md5']}
+            new_md5_df = pd.concat([new_md5_df, pd.DataFrame([new_row])], ignore_index=True)
+
+
+    new_md5_df.to_parquet('./imports_validator/md5.pickle')
 
 
 if __name__ == "__main__":
