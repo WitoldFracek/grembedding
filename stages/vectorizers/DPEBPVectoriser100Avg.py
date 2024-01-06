474c29d7e3151c0d03dcd2a02a8cc7467d0b76ac
Witold Frącek <witek.fracek@gmail.com>
2024-01-06 12:49:22 +0100

    dvc: commit experiment 572a6c1a8c85ca548094c0ff5ef68278a3cbc2b607675d5491a637d1eba7e468

--- stages/vectorizers/DPEBPVectoriser100Avg.py
+++
@@ -1,6 +0,0 @@
-from stages.vectorizers.components.DPEBPVectoriser import DPEBPVectoriser
-
-
-class DPEBPVectoriser100Avg(DPEBPVectoriser):
-    def __init__(self):
-        super().__init__(dim=100, agg_metohd='avg')
