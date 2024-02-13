c2c28bcba65c84903b5500aa0ba365b6a16de97b
Witold Frącek <witek.fracek@gmail.com>
2024-02-13 21:54:43 +0100

    dataloader first look

---
+++ stages/dataloaders/PrusVsSienkiewicz.py
@@ -0,0 +1,56 @@
+import os
+
+import pandas as pd
+import random
+from loguru import logger
+from stages.dataloaders.utils import make_split
+from stages.dataloaders.DataLoader import DataLoader
+
+
+class PrusVsSienkiewicz(DataLoader):
+
+    def __init__(self):
+        super().__init__()
+        self.authors_mapping = {}
+
+    DATASET_DIR: str = 'prus_vs_sienkiewicz'
+
+    def create_dataset(self) -> None:
+        df: pd.DataFrame = pd.read_parquet(
+            os.path.join(self.raw_datasets_dir, self.DATASET_DIR, 'data.parquet')
+        )
+        authors = df['author'].unique()
+        self.authors_mapping = {
+            author: i
+            for i, author
+            in enumerate(sorted(list(authors)))
+        }
+        df['author'] = df['author'].apply(lambda a: self.authors_mapping[a])
+
+    def title_wise_split(self, df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+        X_train = pd.DataFrame(columns=['text', 'label'])
+        X_test = pd.DataFrame(columns=['text', 'label'])
+
+        for author in self.authors_mapping.values():
+            titles = list(df[df['author'] == author]['title'].unique())
+            random.shuffle(titles)
+            split_index = round(len(titles) / 5 * 3)  # 3/5 to train, 2/5 to test
+            train_titles = titles[:split_index]
+            test_titles = titles[split_index:]
+
+            train_titles_df = df[df['title'].isin(train_titles)].copy()
+            train_titles_df.drop(columns=['title'], inplace=True)
+            train_titles_df.rename(columns={'author': 'label'}, inplace=True)
+            X_train = pd.concat([X_train, train_titles_df], ignore_index=True)
+
+            test_titles_df = df[df['title'].isin(test_titles)].copy()
+            test_titles_df.drop(columns=['title'])
+            test_titles_df.rename(columns={'author': 'label'}, inplace=True)
+            X_test = pd.concat([X_test, test_titles_df], ignore_index=True)
+
+        return X_train, X_test
+
+
+
+
+
