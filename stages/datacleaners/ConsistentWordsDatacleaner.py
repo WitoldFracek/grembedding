32391e643a5b5b632c572c02421b1acb5c3ae21e
Rafal Piwnik <rafal.piwnik@gmail.com>
2024-02-15 23:54:27 +0100

    removed old

--- stages/datacleaners/ConsistentWordsDatacleaner.py
+++ stages/datacleaners/ConsistentWordsDatacleaner.py
@@ -7,6 +7,7 @@ class ConsistentWordsDatacleaner(DataCleaner):
     If some escape characters are attached to words like \n, \n\n or \r - separate them
     """
 
+    # TODO - dużo książek ma jakieś zlepione znaki \n \r i inne - warto to chyba oddzielać od słów / albo wgl usuwać
     def clean_data(self, dataset: str) -> None:
         train_df, test_df = self.load_dataset(dataset)
 
---
+++ stages/dataloaders/PAN18PL.py
@@ -0,0 +1,33 @@
+import os
+from typing import Union
+
+import pandas as pd
+
+from stages.dataloaders.DataLoader import DataLoader
+
+
+class PAN18PL(DataLoader):
+
+
+    DATASET_DIR = "pan18pl"
+
+    PL_TRAIN_PROBLEMS = [
+        "problem00007",
+        "problem00008"
+    ]
+
+    PL_TEST_PROBLEMS = [
+        "problem00013",
+        "problem00014",
+        "problem00015",
+        "problem00016"
+    ]
+
+    def __init__(self):
+        super(PAN18PL, self).__init__()
+
+
+    def create_dataset(self) -> None:
+        pass
+
+    def _load_problem_texts(self, ) -> pd.DataFrame:
