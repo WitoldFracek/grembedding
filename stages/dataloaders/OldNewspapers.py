9b45f70a14ee25f89eb945b14f16e772429c789d
Witold Frącek <witek.fracek@gmail.com>
2024-03-10 13:42:22 +0100

    added old newspapers dataset

---
+++ stages/dataloaders/OldNewspapers.py
@@ -0,0 +1,24 @@
+from loguru import logger
+from stages.dataloaders.DataLoader import DataLoader
+import pandas as pd
+import os
+
+
+SEED = 0xC0FFEE
+
+
+class OldNewspapers(DataLoader):
+
+    DATASET_DIR = 'old_newspapers'
+
+    def create_dataset(self) -> None:
+        train_df: pd.DataFrame = pd.read_parquet(
+            os.path.join(self.raw_datasets_dir, self.DATASET_DIR, 'train.parquet')
+        )
+        test_df: pd.DataFrame = pd.read_parquet(
+            os.path.join(self.raw_datasets_dir, self.DATASET_DIR, 'test.parquet')
+        )
+
+        train_df = train_df.drop(columns=['source'])
+        test_df = test_df.drop(columns=['source'])
+        self._save_dataset(train_df, test_df)
