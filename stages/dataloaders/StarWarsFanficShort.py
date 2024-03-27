1e559257fa6112f8d7063102825dd385e4ed6512
Witold Frącek <witek.fracek@gmail.com>
2024-03-27 12:04:16 +0100

    added short star wars fanfic

---
+++ stages/dataloaders/StarWarsFanficShort.py
@@ -0,0 +1,58 @@
+from loguru import logger
+from stages.dataloaders.DataLoader import DataLoader
+import pandas as pd
+import os
+import re
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from stages.dataloaders.StarWarsFanfic import StarWarsFanfic
+
+class StarWarsFanficShort(StarWarsFanfic):
+
+    def create_dataset(self) -> None:
+        nowy_wrog_path = os.path.join(self.raw_datasets_dir, self.DATASET_DIR, 'nowy_wrog.txt')
+        wrog_republiki_path = os.path.join(self.raw_datasets_dir, self.DATASET_DIR, 'wrog_republiki.txt')
+
+        nw_text = self.load_and_clean(nowy_wrog_path)
+        wr_text = self.load_and_clean(wrog_republiki_path)
+
+        splitter = RecursiveCharacterTextSplitter(
+            separators=['—', '\n', '.', ',', '!', '?', '-', ' ', ''],
+            chunk_size=100,
+            chunk_overlap=5,
+        )
+
+        nw_documents: list[Document] = splitter.create_documents([nw_text])
+        wr_documents: list[Document] = splitter.create_documents([wr_text])
+        
+        index = int(len(nw_documents) * self.SPLIT_PERCENT)
+        nw_train_data = {
+            'text': [d.page_content for d in nw_documents[:index]],
+            'label': [0] * index
+        }
+        nw_test_data = {
+            'text': [d.page_content for d in nw_documents[index:]],
+            'label': [0] * (len(nw_documents) - index)
+        }
+        nw_train_df = pd.DataFrame.from_dict(nw_train_data)
+        nw_test_df = pd.DataFrame.from_dict(nw_test_data)
+
+        index = int(len(wr_documents) * self.SPLIT_PERCENT)
+        wr_train_data = {
+            'text': [d.page_content for d in wr_documents[:index]],
+            'label': [1] * index
+        }
+        wr_test_data = {
+            'text': [d.page_content for d in wr_documents[index:]],
+            'label': [1] * (len(wr_documents) - index)
+        }
+        wr_train_df = pd.DataFrame.from_dict(wr_train_data)
+        wr_test_df = pd.DataFrame.from_dict(wr_test_data)
+
+        train_df = pd.concat([nw_train_df, wr_train_df], ignore_index=True)
+        test_df = pd.concat([nw_test_df, wr_test_df], ignore_index=True)
+
+        train_df = train_df.sample(frac=1.0, random_state=0xC0FFEE)
+        test_df = test_df.sample(frac=1.0, random_state=0xC0DE)
+
+        self._save_dataset(train_df, test_df)
