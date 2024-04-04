6fdc8e3324d66471a5d30fd4dc9dc1c2c4091b09
RP <rafal.piwnik@gmail.com>
2024-04-04 17:57:19 +0200

    old newspapers (#28)

--- stages/dataloaders/OldNewspapers.py
+++ stages/dataloaders/OldNewspapers.py
@@ -1,14 +1,14 @@
 from loguru import logger
+from sklearn.model_selection import train_test_split
+
 from stages.dataloaders.DataLoader import DataLoader
 import pandas as pd
 import os
-
 
 SEED = 0xC0FFEE
 
 
 class OldNewspapers(DataLoader):
-
     DATASET_DIR = 'old_newspapers'
 
     def create_dataset(self) -> None:
@@ -24,8 +24,10 @@ class OldNewspapers(DataLoader):
         train_df = train_df.drop(columns=['source'])
         test_df = test_df.drop(columns=['source'])
 
-        # TODO sample this stratified on label
-        train_df = train_df.sample(frac=0.1, random_state=SEED)
-        test_df = test_df.sample(frac=0.1, random_state=SEED)
+        _, train_df = train_test_split(train_df, test_size=0.1, random_state=SEED, stratify=train_df['label'])
+        _, test_df = train_test_split(test_df, test_size=0.1, random_state=SEED, stratify=test_df['label'])
+        logger.debug(f"OldNewspapers has {len(train_df)} train samples and {len(test_df)} test samples. "
+                     f"Avg train text length: {train_df['text'].str.len().mean():.2f}, "
+                     f"Std train text length: {train_df['text'].str.len().std():.2f}")
 
         self._save_dataset(train_df, test_df)
