af19461d27e097d1bd43e1cc7e27277f3ba56750
Michal <mskrzypa0@gmail.com>
2024-04-25 20:22:57 +0200

    stylometrix fixed number of samples

--- stages/vectorizers/StyloMetrix.py
+++ stages/vectorizers/StyloMetrix.py
@@ -1,11 +1,13 @@
 import spacy
 
 from stages.vectorizers.Vectorizer import Vectorizer
+from sklearn.model_selection import train_test_split
 import stylo_metrix as sm
 import numpy as np
 
 from utils.spacy_gpu import autoconfigure_spacy_mode
 
+TRAIN_SAMPLES = 10_000
 
 class StyloMetrix(Vectorizer):
     def __init__(self) -> None:
@@ -14,6 +16,10 @@ class StyloMetrix(Vectorizer):
 
     def vectorize(self, dataset: str, datacleaner: str) -> None:
         df_train, df_test = self.load_train_test_dataframes(dataset, datacleaner)
+
+        ratio = TRAIN_SAMPLES/len(df_train)
+        df_train, _ = train_test_split(df_train, train_size=ratio, stratify=df_train["label"])
+        df_test, _ = train_test_split(df_test, train_size=ratio, stratify=df_test["label"])
 
         stylo = sm.StyloMetrix('pl')
         X_train: np.ndarray = stylo.transform(df_train["clean_text"]).drop(columns="text").to_numpy()
